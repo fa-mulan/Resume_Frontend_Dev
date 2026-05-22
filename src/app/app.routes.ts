ea60be00768423b3c71ac3947fbdfee5921e6011
Alena <fa.mulan2026@gmail.com>
2026-05-22 17:26:54 +0300

    add components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Education } from './education/education';
+import { Skills } from './skills/skills';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "education", component: Education },
+    { path: "skills", component: Skills }
+];
